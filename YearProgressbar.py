c7b567380cb63ccc51ca8b9dcb3c33d194ea421b
Farshadi <asadpoor.f@gmail.com>
2020-11-10 19:28:56 +0330

    add precent to progress bar

--- YearProgressbar.py
+++ YearProgressbar.py
@@ -1,11 +1,15 @@
 import datetime
 
-today=datetime.date.today()
-start_year=datetime.date(2020,1,1)
-end_year=datetime.date(2020,12,30)
-passed_days=(today-start_year).days
-remaining_days=(end_year-today).days
+today = datetime.date.today()
+start_year = datetime.date(2020, 1, 1)
+end_year = datetime.date(2020, 12, 30)
+passed_days = (today - start_year).days
+remaining_days = (end_year - today).days
 # print(passed_days,remaining_days,passed_days+remaining_days)
-passed_days_char='⬛'
-remaining_days_char='⬜'
-print((passed_days_char*int(passed_days/15))+(remaining_days_char*int(remaining_days/15)))
+passed_days_char = '⬛'
+remaining_days_char = '⬜'
+passed_days_progressbar=(passed_days_char * int(passed_days / 15))
+remaining_days_progressbar=(remaining_days_char * int(remaining_days / 15))
+calc_Percent_passed_days=(passed_days/(passed_days+remaining_days)*100)
+calc_Percent_remaining_days=(remaining_days/(passed_days+remaining_days)*100)
+print('%.2f %s%s %.2f'%(calc_Percent_passed_days,passed_days_progressbar,remaining_days_progressbar,calc_Percent_remaining_days))
