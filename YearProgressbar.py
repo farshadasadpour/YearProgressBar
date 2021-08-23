a1116f3391526251b0fbef15d34d9fb32bcba632
asadpoor.f@gmail.com <asadpoor.f@gmail.com>
2021-08-23 12:54:33 +0430

    change

--- YearProgressbar.py
+++ YearProgressbar.py
@@ -12,4 +12,5 @@ passed_days_progressbar=(passed_days_char * int(passed_days / 15))
 remaining_days_progressbar=(remaining_days_char * int(remaining_days / 15))
 calc_Percent_passed_days=(passed_days/(passed_days+remaining_days)*100)
 calc_Percent_remaining_days=(remaining_days/(passed_days+remaining_days)*100)
+
 print('%.2f%s %s%s %.2f%s'%(calc_Percent_passed_days,'%',passed_days_progressbar,remaining_days_progressbar,calc_Percent_remaining_days,'%'))
